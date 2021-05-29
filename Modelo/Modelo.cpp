9e75a569b62e4a6d4f3c29f8d7c04591338ecc54
RVLareu <rlareu@fi.uba.ar>
2021-05-29 20:00:05 -0300

    fueguitos sobre plataforma y velocidad sprite fuegos

--- Modelo/Modelo.cpp
+++ Modelo/Modelo.cpp
@@ -97,26 +97,67 @@ this->modelosPersonajes.push_back( ( new FactoryPersonaje() )->getPersonaje("pla
 
 int posiciones_x[cantidad_fueguitos]={};
 int posiciones_y[cantidad_fueguitos]={};
+
+int plataformas_y[5]={155,420,535};
+std::uniform_int_distribution<int> plat_y(0, 2);
+
+
+int plataformas_x_1_inicio[2] = {200,590};
+int plataformas_x_1_fin[2] = {400,620};
+
+
+int plataformas_x_2_inicio[5] = {0,220,380,530,680};
+int plataformas_x_2_fin[5] = {120,300,450,600,790};
+
+
+int* plataformas_x [3] = {plataformas_x_1_inicio,plataformas_x_2_inicio,plataformas_x_2_inicio};
+
+
 //esto es para la aleatoriedad
 std::random_device rd_x;
 std::random_device rd_y;
-std::uniform_int_distribution<int> dist_x(0, 700);
-std::uniform_int_distribution<int> dist_y(0, 500);
-
+std::uniform_int_distribution<int> dist_x_1(0, 2);
+std::uniform_int_distribution<int> dist_x_2(0, 4);
+int plat_rand_y;
 int posicion_x_aleatoria;
 int posicion_y_aleatoria;
 int i = 0;
 int cantidad_fueguitos_generados = 0;
 int dist=0;
-
+int plataforma;
 while(i<cantidad_fueguitos){
-    posicion_x_aleatoria=dist_x(rd_x);
-    posicion_y_aleatoria=dist_y(rd_y);
+    int plataforma_aleatoria_y = plat_y(rd_y);
+    posicion_y_aleatoria=plataformas_y[plataforma_aleatoria_y];
+    int* plataforma_aleatoria_x=plataformas_x[plataforma_aleatoria_y];
+    if (sizeof(plataforma_aleatoria_x[0]>2)){
+        plataforma = dist_x_2(rd_x);
+        std::uniform_int_distribution<int> distribucion_x_1(plataformas_x_2_inicio[plataforma], plataformas_x_2_fin[plataforma]);
+        posicion_x_aleatoria=distribucion_x_1(rd_x);
+    }
+    else{
+        std::uniform_int_distribution<int> distribucion_x_1(plataformas_x_1_inicio[plataforma], plataformas_x_1_fin[plataforma]);
+        posicion_x_aleatoria=distribucion_x_1(rd_x);
+    }
+
+;
+
+
     dist = 0;
     while(dist<cantidad_fueguitos_generados){
         if((sqrt(pow(posicion_x_aleatoria-posiciones_x[dist],2)+pow(posicion_y_aleatoria-posiciones_y[dist],2)) <= 40) || (sqrt(pow(posicion_x_aleatoria-500,2)+pow(posicion_y_aleatoria-12,2)) <= 130)){
-            posicion_x_aleatoria=dist_x(rd_x);
-            posicion_y_aleatoria=dist_y(rd_y);
+        int plataforma_aleatoria_y = plat_y(rd_y);
+        posicion_y_aleatoria=plataformas_y[plataforma_aleatoria_y];
+        int* plataforma_aleatoria_x=plataformas_x[plataforma_aleatoria_y];
+        if (sizeof(plataforma_aleatoria_x[0]>2)){
+            plataforma = dist_x_2(rd_x);
+            std::uniform_int_distribution<int> distribucion_x_1(plataformas_x_2_inicio[plataforma], plataformas_x_2_fin[plataforma]);
+            posicion_x_aleatoria=distribucion_x_1(rd_x);
+        }
+        else{
+            std::uniform_int_distribution<int> distribucion_x_1(plataformas_x_1_inicio[plataforma], plataformas_x_1_fin[plataforma]);
+            posicion_x_aleatoria=distribucion_x_1(rd_x);
+            }
+
             dist=-1;
         }
         dist++;
@@ -143,26 +184,34 @@ void Modelo::escenario1(int cantidad_fueguitos)
 
 int posiciones_x[cantidad_fueguitos]={};
 int posiciones_y[cantidad_fueguitos]={};
+int rectas_ordenada[4] = {388,350,222,185};
+double rectas_pendiente[4] = {0.06,-0.06,0.06,-0.06};
+
+
 //esto es para la aleatoriedad
 std::random_device rd_x;
 std::random_device rd_y;
+std::random_device platform;
 std::uniform_int_distribution<int> dist_x(0, 700);
-std::uniform_int_distribution<int> dist_y(0, 500);
+std::uniform_int_distribution<int> dist_platform(0,3);
 
+int random_platform;
 int posicion_x_aleatoria;
 int posicion_y_aleatoria;
 int i = 0;
 int cantidad_fueguitos_generados = 0;
 int dist=0;
 
+
 while(i<cantidad_fueguitos){
     posicion_x_aleatoria=dist_x(rd_x);
-    posicion_y_aleatoria=dist_y(rd_y);
+    random_platform =dist_platform(platform);
+    posicion_y_aleatoria=posicion_x_aleatoria*rectas_pendiente[random_platform] + rectas_ordenada[random_platform];
     dist = 0;
     while(dist<cantidad_fueguitos_generados){
         if((sqrt(pow(posicion_x_aleatoria-posiciones_x[dist],2)+pow(posicion_y_aleatoria-posiciones_y[dist],2)) <= 40) || (sqrt(pow(posicion_x_aleatoria-500,2)+pow(posicion_y_aleatoria-12,2)) <= 130)){
             posicion_x_aleatoria=dist_x(rd_x);
-            posicion_y_aleatoria=dist_y(rd_y);
+            posicion_y_aleatoria=posicion_x_aleatoria*rectas_pendiente[random_platform] + rectas_ordenada[random_platform];
             dist=-1;
         }
         dist++;
@@ -179,6 +228,7 @@ while(i<cantidad_fueguitos){
 
 printf("Entre al esceneraio%d\n",cantidad_fueguitos);
  //this->modelosPersonajes=std::vector<Modelo_Jugador*>();
+
 
 
 
@@ -221,13 +271,13 @@ void Modelo::acciones()
         this->modelosPersonajes[i]->caminar() ;
 
         if(this->modelosPersonajes[i]->getNombre()=="fueguito"){
-            if(this->contador<300){
+            if(this->contador<100){
             this->modelosPersonajes[i]->setPosicionX(this->modelosPersonajes[i]->getPosicionX()+1);
         }
         else{
             this->modelosPersonajes[i]->setPosicionX(this->modelosPersonajes[i]->getPosicionX()-1);
         }
-            if (this->contador>600){
+            if (this->contador>200){
                 this->contador = 0;
             }
 
